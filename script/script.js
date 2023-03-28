895965134dcb04a4123188f570f584788b739419
Rishelly Amaya <amayaguio05@gmail.com>
2023-03-29 01:40:20 +0200

    Intento de agregar eventos

--- script/script.js
+++ script/script.js
@@ -21,54 +21,18 @@ function listaDeProductos (){
     <h3 class="py-3 m-1"> ${producto.nombre} <h3/>
     <img src="./assets/imagenes/45069787-retro-sencilla-metálica-vector-blanco-y-negro-estilizado-cesta-de-la-vendimia-completa-con-productos.webp" class="img-thumbnail" alt="imagen de producto">
     <p> $${producto.precio} <p/>
+    <button class="button btn mt-3 p-1 rounded" id="btn${producto.codigo}">AGREGAR AL CARRITO</button>
     `
     let lista = document.getElementById ('listaProductos')
     lista.append(seccion)
+    let btnAgregar = document.getElementById (`btn${producto.codigo}`)
+    btnAgregar.addEventListener ("click", agregarAlCarrito)
+
+    function agregarAlCarrito (){
+         console.log("Me hicieron click") 
+      
+    }
+    agregarAlCarrito ()
  }
 }
- 
 listaDeProductos ()
-/*const carrito = []
-let seguirComprando = true;
-    while(seguirComprando){
-        let productoSeleccionado = parseInt(prompt("Quiero el producto número:"));
-        switch (productoSeleccionado){
-            case 1:
-                carrito.push(productos[0])
-                break;
-
-            case 2: 
-                carrito.push(productos[1])
-                break;
-
-            case 3:
-                carrito.push(productos[2])
-                break;
-                
-            case 4:
-                carrito.push(productos[3])
-                break;
-
-            default:
-                alert("Actualmente el número ingresado no corresponde a ningún producto en stock.")
-                break;
-        }
-        let elegirSeguirComprando = parseInt(prompt ("¿Quiere seguir agregando productos?: 1= Sí, 2= Ir a pagar"))
-        if(elegirSeguirComprando ===1){
-            seguirComprando = true;
-        }else if (elegirSeguirComprando ===2){
-            seguirComprando = false
-        }else (alert("Opción inválida"));
-}*/
-/*
-let total = 0
-carrito.forEach((el) => {
-    total += el.precio
-})
-
-console.log(cliente1, carrito, total)
-alert("El monto total a pagar de su carrito es: $" + total)
-
-window.addEventListener ("load", function(){
-    cliente()
-}) */

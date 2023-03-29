6ed1ef822c94f8eba1e219606fed67db1b356682
Rishelly Amaya <amayaguio05@gmail.com>
2023-03-29 18:46:49 +0200

    Lucha contra el carrito

--- script/script.js
+++ script/script.js
@@ -19,15 +19,13 @@ function validarIdentidad (e){
 }
 
 const productos = [
-    { codigo: 0, nombre: "PRODUCTO 1", precio: 150}, 
-    { codigo: 1, nombre: "PRODUCTO 2", precio: 500},
-    { codigo: 2, nombre: "PRODUCTO 3", precio: 200},
-    { codigo: 3, nombre: "PRODUCTO 4", precio: 250},
-    { codigo: 4, nombre: "PRODUCTO 5", precio: 1050}, 
-    { codigo: 5, nombre: "PRODUCTO 6", precio: 900},
+    { id: 0, nombre: "PRODUCTO 1", precio: 150}, 
+    { id: 1, nombre: "PRODUCTO 2", precio: 500},
+    { id: 2, nombre: "PRODUCTO 3", precio: 200},
+    { id: 3, nombre: "PRODUCTO 4", precio: 250},
+    { id: 4, nombre: "PRODUCTO 5", precio: 1050}, 
+    { id: 5, nombre: "PRODUCTO 6", precio: 900},
 ]
-
-const carrito = []
 
 function listaDeProductos (){
  for (const producto of productos){
@@ -36,26 +34,30 @@ function listaDeProductos (){
     <h3 class="py-3 m-1"> ${producto.nombre} <h3/>
     <img src="./assets/imagenes/45069787-retro-sencilla-metálica-vector-blanco-y-negro-estilizado-cesta-de-la-vendimia-completa-con-productos.webp" class="img-thumbnail" alt="imagen de producto">
     <p> $${producto.precio} <p/>
-    <button class="button btn mt-3 p-1 rounded" id="${producto.codigo}">AGREGAR AL CARRITO</button>
+    <button class="botonAgregar btn mt-3 p-1 rounded" id="${producto.id}">AGREGAR AL CARRITO</button>
     `
 
     seccion.className = 'col-3 m-1 p-1'
     let lista = document.getElementById ('listaProductos')
-    lista.append(seccion)
-    let btnAgregar = document.getElementById (`${producto.codigo}`)
-    btnAgregar.addEventListener ("click", agregarAlCarrito)
-
-    function agregarAlCarrito (e){
-console.log(producto.codigo)
-    }
-    agregarAlCarrito ()
- }
+    lista.append(seccion)}
 }
 listaDeProductos ()
 
-const btnCarrito = document.querySelector('.iconoCarrito')
+const btnCarrito = document.querySelector('.contenedorDelCarrito')
 const containerCarrito = document.querySelector('.contenedor-de-carrito')
 
 btnCarrito.addEventListener('click',() => {
     containerCarrito.classList.toggle('hidden-carrito')
 })
+
+const infoCarrito = document.querySelector('.productoCarrito')
+const filaProducto = document.querySelector('.filaDeProducto')
+
+let stock = document.getElementById('listaProductos')
+let carrito = []
+
+stock.addEventListener('click', e=> {
+    if(e.target.classList.contains('botonAgregar')){
+            console.log(e.target.id)
+    }
+} )
